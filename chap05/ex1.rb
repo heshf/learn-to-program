16c8ac485f32fec1cfa0da973ad098b797e94502
Hesh <heshamf@gmail.com>
2017-06-15 22:28:13 +0100

    adding chap05

---
+++ chap05/ex1.rb
@@ -0,0 +1,12 @@
+puts "What is your first name?"
+f_name = gets.chomp
+
+puts "What is your middle name?"
+m_name = gets.chomp
+
+puts "What is your last name?"
+l_name = gets.chomp
+
+puts "Hello, " + f_name +" " +m_name +" "+ l_name
+
+ 
---
+++ chap05/ex2.rb
@@ -0,0 +1,5 @@
+puts "Hello there, what is your favourite number?"
+num = gets.chomp.to_i
+#num.to_i
+
+puts "I think #{num + 1} is a better number"
