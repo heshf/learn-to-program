bfe5e23a599870ee468442450785730de1285766
Hesh <heshamf@gmail.com>
2017-06-16 10:46:27 +0100

    Adding chap06

--- notes.rb
+++ notes.rb
@@ -1,4 +1,14 @@
-puts "Hello there, and what's your name?"
-name = gets.chomp
-puts "Your name is " + name + "? What a lovely name!"
-puts "Pleased to meet you, " + name + ". :)"
+=begin puts "what is your first name?"
+f_name = gets.chomp.length
+
+puts "what is your middle name?"
+m_name=gets.chomp.length
+
+puts "what is your last name?"
+l_name = gets.chomp.length
+
+full_name=(f_name+l_name+m_name).to_s
+
+puts "Did you know there are " + full_name+ " characters in your name?"
+=end
+puts rand(100000000)
